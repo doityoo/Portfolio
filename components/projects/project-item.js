b8265e29293adfa7024f95125a6443c4314407c5
doityoo <yitsky@naver.com>
2023-03-29 13:40:35 +0900

    feat: projects notion api 데이터 CSS 작성

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -18,7 +18,7 @@ const ProjectItem = (props) => {
         <a>
           <div className={classes.image}>
             <Image
-              src={cover.file? cover.file.url : cover.external.url}
+              src={cover.file ? cover.file.url : cover.external.url}
               alt={properties.이름.title[0].plain_text}
               width={300}
               height={200}
@@ -28,12 +28,23 @@ const ProjectItem = (props) => {
           </div>
           <div className={classes.content}>
             <h4>{properties.이름.title[0].plain_text}</h4>
-            <time></time>
-            <p>{properties.Description.rich_text[0].plain_text}</p>
+            {properties.Stack.multi_select.map((stack) => {
+              return <span style={{
+                backgroundColor: stack.color,
+                display: 'inline-block',
+                color: 'white',
+                borderRadius: '6px',
+                marginRight: '2%',
+                padding: '1%',
+                fontSize: '0.7rem'
+              }}>{stack.name}</span>
+            })}
+            <div className={classes.members}>{properties.Members.rich_text[0].text.content}</div>
+            <p className={classes.dsc}>{properties.Description.rich_text[0].plain_text}</p>
           </div>
         </a>
       </Link>
-    </li>
+    </li >
   );
 };
 
