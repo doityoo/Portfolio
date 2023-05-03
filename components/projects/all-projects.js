661f5cb5b60e575a2e3cd2eec26ecdfe1b9c85a0
doityoo <yitsky@naver.com>
2023-05-03 15:40:30 +0900

    fix: pages - projects dir 명 수정

--- components/projects/all-projects.js
+++ components/projects/all-projects.js
@@ -1,12 +1,11 @@
 import classes from './all-projects.module.css'
 import ProjectGrid from './project-grid';
-const AllProjects = (props) => {
 
+const AllProjects = (props) => {
   return (
     <section className={classes.projects}>
       <h1>All Projects</h1>
       <ProjectGrid projects={props.projects} />
-
     </section>
   );
 };
