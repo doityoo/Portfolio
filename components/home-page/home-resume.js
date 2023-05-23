73dcb131bd444d684313115c781c7ac920a606eb
doityoo <yitsky@naver.com>
2023-05-23 15:32:14 +0900

    feat: iframe 내에 url을 새 브라우저 창으로 열기 처리

--- components/home-page/home-resume.js
+++ components/home-page/home-resume.js
@@ -2,11 +2,21 @@ import classes from './home-resume.module.css'
 import Link from "next/link";
 
 const HomeResume = () => {
+  const handleIframeLoad = () => {
+    const iframeDocument = document.getElementById('myIframe').contentDocument;
+    const links = iframeDocument.getElementsByTagName('a');
+    for (let i = 0; i < links.length; i++) {
+      links[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        window.open(this.href, '_blank');
+      });
+    }
+  };
   return (
     <section className={classes.latest} >
       <h3>RESUME</h3>
       <div className={classes.gradiention}>
-        <iframe className={classes.iframe} src="/resume.html" width="100%" height="900px" style={{ border: 'none' }} scrolling="no" target="_blank" />
+        <iframe className={classes.iframe} src="/resume.html" width="100%" height="900px" style={{ border: 'none' }} scrolling="no" target="_blank" onLoad={handleIframeLoad} />
         <button className={classes.addButton}>
           <Link href='/resume'>
             ● ● ●
--- components/resume/resume.js
+++ components/resume/resume.js
@@ -1,12 +1,35 @@
 // import ProjectGrid from '../projects/project-grid';
 import classes from './resume.module.css'
+import React, { useRef } from 'react';
 
 const Resume = () => {
+  const iframeRef = useRef(null);
+
+  const handleIframeLoad = () => {
+    const iframeDocument = iframeRef.current.contentDocument;
+    if (iframeDocument) {
+      const links = iframeDocument.getElementsByTagName('a');
+      for (let i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', function (event) {
+          event.preventDefault();
+          window.open(this.href, '_blank');
+        });
+      }
+    }
+  };
   return (
     <section className={classes.latest} >
       <h3>RESUME</h3>
-      <iframe src="/resume.html" width="100%" height="3500px" style={{ border: 'none' }} scrolling="no" target="_blank" />
-
+      <iframe
+        ref={iframeRef}
+        src="/resume.html"
+        width="100%"
+        height="3500px"
+        style={{ border: 'none' }}
+        scrolling="no"
+        target="_blank"
+        onLoad={handleIframeLoad}
+      />
     </section>
   );
 };
