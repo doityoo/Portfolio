6155996983fe570a3781027123f7e632ea95e400
doityoo <yitsky@naver.com>
2023-05-23 20:36:46 +0900

    style: button, header style 수정

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import classes from './main-navigation.module.css'
 
 const MainNavigation = () => {
+  const router = useRouter();
+
   return (
     <header className={classes.header}>
       <nav>
         <ul>
-          <li><Link href='/'>HOME</Link></li>
-          <li><Link href='/resume'>RESUME</Link></li>
-          <li><Link href='/projects'>PROJECTS</Link></li>
-          <li><Link href='/contact'>CONTACT</Link></li>
+          <li><Link href='/'><a className={router.pathname === '/' ? classes.activeLink : ''}>HOME</a></Link></li>
+          <li><Link href='/resume'><a className={router.pathname === '/resume' ? classes.activeLink : ''}>RESUME</a></Link></li>
+          <li><Link href='/projects'><a className={router.pathname === '/projects' ? classes.activeLink : ''}>PROJECTS</a></Link></li>
+          <li><Link href='/contact'><a className={router.pathname === '/contact' ? classes.activeLink : ''}>CONTACT</a></Link></li>
         </ul>
       </nav>
     </header>
