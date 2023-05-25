dfb3ead8451ee34e1a9023e8a156e2c38dc5e215
doityoo <yitsky@naver.com>
2023-05-25 13:53:17 +0900

    style: resume style 수정 / index 반응형 처리

--- components/resume/resume.js
+++ components/resume/resume.js
@@ -24,7 +24,7 @@ const Resume = () => {
         ref={iframeRef}
         src="/resume.html"
         width="100%"
-        height="3500px"
+        height="4300px"
         style={{ border: 'none' }}
         scrolling="no"
         target="_blank"
