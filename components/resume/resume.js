b8177b55fcbe58268db6df56bcfcbb4e6a4f956f
doityoo <yitsky@naver.com>
2023-05-25 21:21:50 +0900

    style: 모바일 사이즈 반응형 수정

--- components/resume/resume.js
+++ components/resume/resume.js
@@ -1,6 +1,5 @@
-// import ProjectGrid from '../projects/project-grid';
-import classes from './resume.module.css'
-import React, { useRef } from 'react';
+import classes from './resume.module.css';
+import React, { useRef, useEffect } from 'react';
 
 const Resume = () => {
   const iframeRef = useRef(null);
@@ -17,14 +16,44 @@ const Resume = () => {
       }
     }
   };
+
+  useEffect(() => {
+    const updateIframeHeight = () => {
+      const iframe = iframeRef.current;
+      const width = window.innerWidth;
+
+      if (width >= 1000) {
+        iframe.style.height = '4200px';
+      } else if (width >= 800) {
+        iframe.style.height = '4400px';
+      } else if (width >= 700) {
+        iframe.style.height = '4600px';
+      } else if (width >= 600) {
+        iframe.style.height = '4940px';
+      } else if (width >= 500) {
+        iframe.style.height = '5400px';
+      } else if (width >= 420) {
+        iframe.style.height = '6100px';
+      } else if (width >= 350) {
+        iframe.style.height = '6800px';
+      }
+    };
+
+    updateIframeHeight();
+
+    window.addEventListener('resize', updateIframeHeight);
+    return () => {
+      window.removeEventListener('resize', updateIframeHeight);
+    };
+  }, []);
+
   return (
-    <section className={classes.latest} >
+    <section className={classes.latest}>
       <h3>RESUME</h3>
       <iframe
         ref={iframeRef}
         src="/resume.html"
         width="100%"
-        height="4300px"
         style={{ border: 'none' }}
         scrolling="no"
         target="_blank"
