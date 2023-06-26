267226a088aed2d6c880bc664504b03adb2d63b7
doityoo <yitsky@naver.com>
2023-06-26 22:39:25 +0900

    fix: ourselves text 수정

--- components/home-page/ourselves.js
+++ components/home-page/ourselves.js
@@ -5,9 +5,10 @@ const Ourselves = () => {
     <section className={classes.ourselves}>
       <h1>프론트엔드 개발자, <br />
         유인태입니다.</h1>
-      <h2>개발자로서 어떻게 '생각'하고 '일' 하는가?</h2>
-      <div><strong>‘성장’</strong>과 <strong>‘변화’</strong>를 지속적으로 <strong>생각하며</strong> </div>
-      <div><strong>‘책임’</strong>과 <strong>‘존중’</strong>의 태도로 일합니다.</div>
+      {/* <h2>개발자로서 어떻게 '생각'하고 '일' 하는가?</h2> */}
+      <div>개발을 하며 직면한 문제를 피하지 않습니다.</div>
+      <div>문제 해결을 위해 수많은 시행착오를 겪지만 이 과정을 즐기며, </div>
+      <div>이를 토대로 유의미한 성장을 이뤄갑니다. </div>
       <br />
       <br />
       <div><strong>JavaScript, TypeScript, React, Next.JS</strong> 등 프로젝의 경험이 있습니다.</div>
@@ -16,3 +17,4 @@ const Ourselves = () => {
 };
 
 export default Ourselves;
+
