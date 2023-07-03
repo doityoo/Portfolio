0aa0bd94830a4fa02752f19c133ff0bd2c253df0
doityoo <yitsky@naver.com>
2023-07-03 20:40:43 +0900

    fix: ourselves text 내용 수정

--- components/home-page/ourselves.js
+++ components/home-page/ourselves.js
@@ -6,12 +6,12 @@ const Ourselves = () => {
       <h1>프론트엔드 개발자, <br />
         유인태입니다.</h1>
       {/* <h2>개발자로서 어떻게 '생각'하고 '일' 하는가?</h2> */}
-      <div>개발을 하며 직면한 문제를 피하지 않습니다.</div>
+      {/* <div>개발을 하며 직면한 문제를 피하지 않습니다.</div>
       <div>문제 해결을 위해 수많은 시행착오를 겪지만 이 과정을 즐기며, </div>
-      <div>이를 토대로 유의미한 성장을 이뤄갑니다. </div>
+      <div>이를 토대로 유의미한 성장을 이뤄갑니다. </div> */}
       <br />
       <br />
-      <div><strong>JavaScript, TypeScript, React, Next.JS</strong> 등 프로젝의 경험이 있습니다.</div>
+      <div><strong>JavaScript, TypeScript, React, Next.JS</strong>으로 개발합니다.</div>
     </section>
   );
 };
